783afe5a6327eac14f02d4cd3372be01483934f3
Y0z64 <yairprogrammer@gmail.com>
2024-04-03 20:34:44 -0600

    Finished calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,42 +1,70 @@
-let operations = "";
+let operation = "";
 
 function addToOperationsBox(prop, component) {
   if (/^[0-9().]$/.test(prop)) {
-    operations += prop;
+    operation += prop;
   } else {
-    operations += " " + prop + " ";
+    operation += " " + prop + " ";
   }
-  component.innerText = operations;
+
+  // Remove extra operators
+  operation = operation.replace(/\s*([+*-\/])\s+/g, " $1 ");
+  component.value = operation;
 }
 
 function resolve(component) {
-  let operation = component.innerText;
+  let currentOperation = component.value;
+  // In case we need a history of operations
+  addHistory(currentOperation, operationsBox);
 
-  if (operation === "") {
+  if (currentOperation === "") {
     component.innerText("Nothing here");
     return;
   }
 
-  operation = operation.replace(/x/g, "*");
-  operation.replace();
-  const result = eval(operation);
+  currentOperation = currentOperation.replace(/x/g, "*");
+  currentOperation = currentOperation.replace(/\^/g, "**");
+  const result = eval(currentOperation);
 
-  operations = String(result);
-  component.innerText = result;
+  // In case we need a history of values
+  // operationHistory.push(String(result))
+  operation = String(result);
+  component.value = result;
 }
 
 function cancel(component) {
-  component.innerText = "";
-  operations = "";
+  component.value = "";
+  operation = "";
+}
+
+function addHistory(currentOperation, operationsBox) {
+  let history = document.querySelector(".history");
+
+  if (!history) {
+    history = document.createElement("div");
+    history.classList.add("history");
+    wrapper.appendChild(history);
+  }
+  const historyButton = document.createElement("button");
+  historyButton.classList.add("history-button");
+  historyButton.innerText = currentOperation;
+  history.appendChild(historyButton);
+
+  historyButton.onclick = function () {
+    operationsBox.value = historyButton.innerText;
+    operation = historyButton.innerText;
+    return false; 
+  }
 }
 
 //HTML structure
 const calculator = document.getElementById("calculator");
-const operationsBox = document.createElement("div");
+const operationsBox = document.createElement("textarea");
 const operatorButtons = document.getElementsByClassName("operator-button");
 const numberButtons = document.getElementsByClassName("calc-button");
 const sendButton = document.getElementById("send");
 const cancelButton = document.getElementById("cancel");
+const wrapper = document.getElementById("wrapper");
 
 //Content
 operationsBox.classList.add("operations-box");
@@ -50,7 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (
       button.id !== "cancel" &&
       button.id !== "send" &&
-      button.id !== "rick-roll"&&
       (button.classList.contains("operator-button") ||
         button.classList.contains("calc-button"))
     ) {
@@ -69,4 +96,4 @@ sendButton.onclick = function () {
 cancelButton.onclick = function () {
   cancel(operationsBox);
   return false;
-}
+};
