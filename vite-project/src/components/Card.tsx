afee0a3b0e298387d616b14f35ecbb04416ffdb6
Y0z64 <yairprogrammer@gmail.com>
2024-04-15 21:25:16 -0600

    Added fight scene pokemon

--- vite-project/src/components/Card.tsx
+++ vite-project/src/components/Card.tsx
@@ -1,14 +1,23 @@
-import { Pokemon } from "./Screen"
+import { Pokemon } from "./Screen";
+import { forwardRef, HTMLAttributes, RefAttributes } from "react";
 
-type Props = {
-  pokemon: Pokemon;
-}
+type Props = HTMLAttributes<HTMLDivElement> &
+  RefAttributes<HTMLDivElement> & {
+    pokemon: Pokemon;
+    handleSelectecPokemon: (pokemon: Pokemon) => void;
+  };
 
-export default function Card({pokemon}: Props) {
+const Card = forwardRef<HTMLDivElement, Props>(({ pokemon, handleSelectecPokemon, ...props }, ref) => {
   return (
-    <div className="poke-card">
-      <img src={pokemon.sprite} alt={pokemon.name + " sprite"} className="poke-sprite"/>
+    <div className="poke-card" ref={ref} onClick={() => handleSelectecPokemon(pokemon)} {...props}>
+      <img
+        src={pokemon.sprites.frontSprite}
+        alt={pokemon.name + " sprite"}
+        className="poke-sprite"
+      />
       <p className="poke-name">{pokemon.name}</p>
     </div>
-  )
-}
+  );
+});
+
+export default Card;
--- vite-project/src/components/Screen.tsx
+++ vite-project/src/components/Screen.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Card from "./Card";
+import Battle from "./Battle";
 
 type Response = {
   count: number;
+  next: string;
   results: {
     name: string;
     url: string;
@@ -11,7 +13,10 @@ type Response = {
 
 export type Pokemon = {
   name: string;
-  sprite: string;
+  sprites: {
+    frontSprite: string;
+    backSprite: string;
+  };
   id: number;
 };
 
@@ -19,12 +24,16 @@ type PokemonResponse = {
   id: number;
   sprites: {
     front_default: string;
+    back_default: string;
   };
 };
 
 export default function Screen() {
-  const limit: number = 9
+  const limit: number = 9;
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
+  const lastPokemonElementRef = useRef<HTMLDivElement | null>(null);
 
   const pokeUrl = "https://pokeapi.co/api/v2/pokemon?limit=" + limit;
 
@@ -41,30 +50,75 @@ export default function Screen() {
         console.log("ExtendedPokemon: ", extendedPokemon);
         return {
           name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
-          sprite: extendedPokemon.sprites.front_default,
+          sprites: {
+            frontSprite: extendedPokemon.sprites.front_default,
+            backSprite: extendedPokemon.sprites.back_default,
+          },
           id: extendedPokemon.id,
         };
       })
     );
 
     console.log(pokemons);
-    return pokemons;
+    return { pokemons, next: data.next };
   };
+
+  const loadMore = useCallback(async () => {
+    if (nextUrl) {
+      const { pokemons, next } = await fetchData(nextUrl);
+      setPokemons((prevPokemons) => [...prevPokemons, ...pokemons]);
+      setNextUrl(next);
+    }
+  }, [nextUrl]);
 
   useEffect(() => {
     (async () => {
-      const pokemons = await fetchData(pokeUrl);
+      const { pokemons, next } = await fetchData(pokeUrl);
       setPokemons(pokemons);
+      setNextUrl(next);
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    let observerRefValue = null;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadMore();
+        }
+      },
+      { threshold: 1 }
+    );
+    if (lastPokemonElementRef.current) {
+      observer.observe(lastPokemonElementRef.current);
+      observerRefValue = lastPokemonElementRef.current;
+    }
+    return () => {
+      if (observerRefValue) {
+        observer.unobserve(observerRefValue);
+      }
+    };
+  }, [loadMore]);
+
+  const handleSelectedPokemon = (pokemon: Pokemon) => {
+    setSelectedPokemon(pokemon);
+  };
 
   return (
     <div id="screen" className="bordered">
-      {pokemons.length > 0 ? (
-        pokemons.map((pokemon: Pokemon) => (
-          <Card pokemon={pokemon} />
-        ))
+      {selectedPokemon ? (
+        <Battle chosenPokemon={selectedPokemon} />
+      ) : pokemons.length > 0 ? (
+        <div className="poke-list-wrapper">
+          {pokemons.map((pokemon: Pokemon, index: number) => (
+            <Card
+              pokemon={pokemon}
+              ref={index === pokemons.length - 1 ? lastPokemonElementRef : null}
+              handleSelectecPokemon={handleSelectedPokemon}
+            />
+          ))}
+        </div>
       ) : (
         <h3 className="loading">Loading...</h3>
       )}
